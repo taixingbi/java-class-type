fc10bd6eec5f93824c75c7095cdbaa0d8b3814f6
Taixing Bi <taixingbi@taixings-mbp.lan>
2022-02-24 08:39:00 -0500

    first commit

---
+++ src/main/java/hello/App.java
@@ -0,0 +1,32 @@
+package hello;
+
+
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("----------app start----------");
+        List<String> upcListInput = new ArrayList<>();
+        upcListInput.add("1234");
+        String upcStrInput = "1234";
+
+        Map<String, Object> upcMap = new HashMap<>();
+        upcMap.put("key", upcListInput);
+
+        String name = upcMap.get("key").getClass().getName();
+        if( name.equals("java.lang.String")){
+            System.out.println( name );
+            System.out.println( upcMap.get("key") );
+
+        }
+
+        if( name.equals("java.util.ArrayList")){
+            System.out.println( name );
+            List<String> upcList = (List<String>)upcMap.get("key");
+            String upc = upcList.stream().findFirst().orElse("");
+            System.out.println(upc );
+        }
+
+        System.out.println("----------app end----------");
+    }
+}
